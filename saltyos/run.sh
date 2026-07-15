ec89627d765752d951e6fff30010ac6cca8c461d
sfarthing <sfarthing001@mail.com>
2026-07-15 21:01:07 +1000

    Create Blank Disk image instead of using seed image

--- saltyos/run.sh
+++ saltyos/run.sh
@@ -2,14 +2,17 @@
 set -euo pipefail
 
 PERSISTENT_DISK="/data/disk.img"
-SEED_DISK="/saltyos/disk.img.seed"
+# SEED_DISK="/saltyos/disk.img.seed"
+SEED_DISK="32M"
 
 echo "Starting SaltyOS add-on..."
 
 if [ ! -f "${PERSISTENT_DISK}" ]; then
     echo "No persistent disk image found."
     echo "Creating /data/disk.img from bundled seed disk..."
-    cp "${SEED_DISK}" "${PERSISTENT_DISK}"
+    # cp "${SEED_DISK}" "${PERSISTENT_DISK}"
+    rm -f "${PERSISTENT_DISK}"
+    qemu-img create -f raw "${PERSISTENT_DISK}" "${DISK_SIZE}"   
 fi
 
 echo "Using persistent disk: ${PERSISTENT_DISK}"
